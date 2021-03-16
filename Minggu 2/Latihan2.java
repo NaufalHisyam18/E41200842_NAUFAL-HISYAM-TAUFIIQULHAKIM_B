02295290d2e7cf36b15ef88ef05ce9d244d9aec1
unknown <80368878+NaufalHisyam18@users.noreply.github.com>
2021-03-16 18:45:46 +0700

    Create Latihan2.java

---
+++ Minggu 2/Latihan2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author naufal
+ */
+public class Latihan2 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nama = "Naufal Hisyam";
+        char abjad = 'n';
+        int n = 20;
+        double m = 20.5;
+        
+        System.out.println(nama);
+        System.out.println(n);
+    }
+}
