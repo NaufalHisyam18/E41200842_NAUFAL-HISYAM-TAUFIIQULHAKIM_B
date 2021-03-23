d23deaf8092d54e2575870a8c725ad3013a67dac
unknown <80368878+NaufalHisyam18@users.noreply.github.com>
2021-03-23 14:27:04 +0700

    Create Tugas.java

---
+++ Minggu 3/Tugas/Tugas.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas;
+
+/**
+ *
+ * @author naufal
+ */
+public class Tugas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {  
+      String[] nama = {"Sugito","Prayoga","Rahardika"};{
+    
+    }
+    for(int i=0; i<nama.length; i++){
+        System.out.print(nama[i]);
+        System.out.print(" ");
+      }
+    }
+}
