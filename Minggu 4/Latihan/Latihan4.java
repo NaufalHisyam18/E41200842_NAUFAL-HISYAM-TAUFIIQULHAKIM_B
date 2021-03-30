8dfbea9740cbb704f19d0a6d367ef4906feb3c0c
unknown <80368878+NaufalHisyam18@users.noreply.github.com>
2021-03-30 19:24:53 +0700

    Create Latihan4.java

---
+++ Minggu 4/Latihan/Latihan4.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author naufal
+ */
+public class Latihan4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //membuat array
+        int angka[] = {3, 1, 42, 24, 12};
+
+        //menggunakan perulangan for each untuk menampilkan angka 
+        for (int x : angka) {
+            System.out.print(x + " ");
+        }
+    }
+}
