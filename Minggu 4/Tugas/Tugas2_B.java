ff74a4dc9237099313d7e3df38ab8f88e68a16ef
unknown <80368878+NaufalHisyam18@users.noreply.github.com>
2021-04-04 18:40:02 +0700

    Create Tugas2_B.java

---
+++ Minggu 4/Tugas/Tugas2_B.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas;
+
+/**
+ *
+ * @author naufal
+ */
+public class Tugas2_B {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("                   For                     ");
+        System.out.println("Menampilkan Hasil Perkalian 2 Antara 0-100 ");
+        System.out.println("===========================================");
+        
+  
+       int r = 2;
+       
+       for (int q = 0; q <= 50; q++){
+           int hasil = q * r;
+        System.out.print( hasil + " ");
+       }
+    }
+    
+}
