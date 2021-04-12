76fa1a06941fc133122945b659ed4784ce48830e
unknown <80368878+NaufalHisyam18@users.noreply.github.com>
2021-04-12 09:00:14 +0700

    Create Vertex.java

---
+++ Minggu 5/Minggu5_Graph/Vertex.java
@@ -0,0 +1,14 @@
+package Minggu5_Graph;
+
+/**
+ *
+ * @author naufal
+ */
+public class Vertex {
+    public char label;
+    public boolean wasVisited;
+    
+    public Vertex (char label) {
+        this.label = label; wasVisited = false;
+    }
+}
