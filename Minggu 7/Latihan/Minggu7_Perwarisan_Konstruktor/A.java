ade6d6baf28b26a1165f074fbbc0f91c1d841b8a
unknown <80368878+NaufalHisyam18@users.noreply.github.com>
2021-04-27 14:19:47 +0700

    Create A.java

---
+++ Minggu 7/Latihan/Minggu7_Perwarisan_Konstruktor/A.java
@@ -0,0 +1,11 @@
+package Minggu7_Perwarisan_Konstruktor;
+
+/**
+ *
+ * @author naufal
+ */
+public class A {
+    A(String param1){
+	System.out.println("Konstruktor Class A dengan  Parameter " + param1 + "dieksekusi ...");
+    }
+}
