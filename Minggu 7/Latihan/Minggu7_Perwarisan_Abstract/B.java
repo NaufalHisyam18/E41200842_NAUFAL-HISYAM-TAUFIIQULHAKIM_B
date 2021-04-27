af6cc7a8341c18ed5c23bf79d3f8b91e65efc751
unknown <80368878+NaufalHisyam18@users.noreply.github.com>
2021-04-27 14:19:40 +0700

    Create B.java

---
+++ Minggu 7/Latihan/Minggu7_Perwarisan_Abstract/B.java
@@ -0,0 +1,11 @@
+package Minggu7_Perwarisan_Abstract;
+
+/**
+ *
+ * @author naufal
+ */
+public class B extends A{
+    public void method2(){ //Method abstrak di-override dan dibuat
+	System.out.println("Method abstrak yang sudah menjadi konkrit dalam Class B");
+    }
+}
