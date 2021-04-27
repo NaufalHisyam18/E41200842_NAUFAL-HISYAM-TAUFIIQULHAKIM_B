8f3cda10f5e52a72baed22bf957d0f8fa9268e92
unknown <80368878+NaufalHisyam18@users.noreply.github.com>
2021-04-27 14:19:54 +0700

    Create Demo.java

---
+++ Minggu 7/Latihan/Minggu7_Perwarisan_Konstruktor/Demo.java
@@ -0,0 +1,12 @@
+package Minggu7_Perwarisan_Konstruktor;
+
+/**
+ *
+ * @author naufal
+ */
+public class Demo {
+   public static void main(String[] args) {
+       B subOb = new B();
+   }
+    
+}
