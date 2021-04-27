f02a5e3f47faf14bbfb60341f186c12c7786c151
unknown <80368878+NaufalHisyam18@users.noreply.github.com>
2021-04-27 14:20:45 +0700

    Create User.java

---
+++ Minggu 7/Tugas Tambahan/Minggu7_Televisi1/User.java
@@ -0,0 +1,27 @@
+package Minggu7_Televisi1;
+/**
+ *
+ * @author naufal
+ */
+public class User {
+    public static void main(String[] args) {
+        Televisi tv = new Televisi("TV Tempoe Doeloe, 14 inches", 10);
+        String [] semuaChannel = {"RCTI","SCTV","INDOSIAR","LA TV",
+                                  "METRO TV","TRANS TV","TPI","TV 7",
+                                  "TVRI","TV G","ANTV"};
+        String [] channelFavorit = {"RCTI","SCTV","INDOSIAR","LA TV",
+                                  "METRO TV","TRANS TV","TPI","TV 7"};
+
+        System.out.println("Naufal, membeli Televisi : "+tv.getDeskripsi());
+
+        tv.getChannels();
+        tv.setChannels(semuaChannel);
+        tv.setChannels(channelFavorit);
+
+        tv.setChannelAktif(3);
+        tv.setVolume(10);
+        tv.setChannelAktif(10);
+        tv.setChannelAktif(1);
+    }
+    
+}
