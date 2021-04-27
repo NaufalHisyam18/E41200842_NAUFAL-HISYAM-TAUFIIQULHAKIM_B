40f5efde79135c16926d8591e00e6634d96c35eb
unknown <80368878+NaufalHisyam18@users.noreply.github.com>
2021-04-27 14:19:51 +0700

    Create B.java

---
+++ Minggu 7/Latihan/Minggu7_Perwarisan_Konstruktor/B.java
@@ -0,0 +1,12 @@
+package Minggu7_Perwarisan_Konstruktor;
+
+/**
+ *
+ * @author naufal
+ */
+public class B extends A{
+    B(){
+	super("test"); //tambahan
+	System.out.println("Konstruktor Class B dieksekusi ...");
+    }
+}
