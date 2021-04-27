39412edc08dfca723537fb2219b19df5e938db45
unknown <80368878+NaufalHisyam18@users.noreply.github.com>
2021-04-27 14:20:12 +0700

    Create Test.java

---
+++ Minggu 7/Latihan/Minggu7_Perwarisan_Orang/Test.java
@@ -0,0 +1,13 @@
+package Minggu7_Perwarisan_Orang;
+
+/**
+ *
+ * @author naufal
+ */
+public class Test {
+    public static void main(String[] args) {
+        Pelajar mahasiswa = new Pelajar("Naufal", 175, 55,
+	"E41200842", "Politeknik Negeri Jember", 98.8);
+        System.out.println(mahasiswa.toString());
+    }
+}
