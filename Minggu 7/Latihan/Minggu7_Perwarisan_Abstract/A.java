b49b8c987abc734272c70425b711b0e5a283978e
unknown <80368878+NaufalHisyam18@users.noreply.github.com>
2021-04-27 14:19:36 +0700

    Create A.java

---
+++ Minggu 7/Latihan/Minggu7_Perwarisan_Abstract/A.java
@@ -0,0 +1,13 @@
+package Minggu7_Perwarisan_Abstract;
+
+/**
+ *
+ * @author naufal
+ */
+public abstract class A {
+    abstract public void method2(); //Deklarasi method abstrak.
+    //method konkrit yang memiliki implementasi
+    public void method1(){
+            System.out.println("Method konkrit dari Class A");
+    }
+}
