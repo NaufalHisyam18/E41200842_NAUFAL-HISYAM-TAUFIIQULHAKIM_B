8149d5dad4a31b97da71f4a6e85b620bb734d41c
unknown <80368878+NaufalHisyam18@users.noreply.github.com>
2021-04-27 14:19:57 +0700

    Create A.java

---
+++ Minggu 7/Latihan/Minggu7_Perwarisan_Nilai/A.java
@@ -0,0 +1,13 @@
+package Minggu7_Perwarisan_Nilai;
+/**
+ *
+ * @author naufal
+ */
+public class A {
+    int x;
+    int y;
+
+    void TampilkanNilaiXY(){
+	System.out.println("NilaiX : " + x + ", y : " + y);
+    }
+}
