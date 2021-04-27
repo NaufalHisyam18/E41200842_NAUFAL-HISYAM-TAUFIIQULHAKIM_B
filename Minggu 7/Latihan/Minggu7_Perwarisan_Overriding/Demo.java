9d5c6ab022c641c9c42b89157be31c262acdd020
unknown <80368878+NaufalHisyam18@users.noreply.github.com>
2021-04-27 14:20:20 +0700

    Create Demo.java

---
+++ Minggu 7/Latihan/Minggu7_Perwarisan_Overriding/Demo.java
@@ -0,0 +1,13 @@
+package Minggu7_Perwarisan_Overriding;
+
+/**
+ *
+ * @author naufal
+ */
+public class Demo {
+    public static void main(String[] args) {
+        B subOb = new B();
+        subOb.tampilkanKeLayar();
+    }
+    
+}
