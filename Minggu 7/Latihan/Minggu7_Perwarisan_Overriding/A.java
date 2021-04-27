dc7f3c14aba4bc4a66bca434649804094f99c685
unknown <80368878+NaufalHisyam18@users.noreply.github.com>
2021-04-27 14:20:15 +0700

    Create A.java

---
+++ Minggu 7/Latihan/Minggu7_Perwarisan_Overriding/A.java
@@ -0,0 +1,11 @@
+package Minggu7_Perwarisan_Overriding;
+
+/**
+ *
+ * @author naufal
+ */
+public class A {
+    public void tampilkanKeLayar(){
+	System.out.println("Method milik Class A dipanggil");
+    }
+}
